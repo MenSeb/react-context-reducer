5286be6846ad0550ee97315192132a17a1d23241
MenSeb <sebastien.menard.github@gmail.com>
2020-12-08 13:47:12 -0500

    update demo

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,60 @@
 import React from 'react';
 import { render } from 'react-dom';
 import * as Context from '~';
+import './styles.scss';
+
+function toggle(state) {
+    const { loading } = state;
+
+    return { ...state, loading: !loading };
+}
+
+function config(state) {
+    const { loading } = state;
+
+    return loading ? state : toggle(state);
+}
+
+const actions = { toggle };
+
+const initial = { loading: false };
+
+function LoadingSpinner() {
+    const { state } = Context.useContext();
+
+    return (
+        <div
+            className="loading-spinner"
+            data-loading={state.loading.toString()}
+        />
+    );
+}
 
 function Demo() {
     return (
-        <Context.Provider>
-            <div id="demo" />;
+        <Context.Provider actions={actions} config={config} initial={initial}>
+            <Context.Consumer>
+                {({ api }) => (
+                    <>
+                        <h1 className="title">React Context</h1>
+                        <p className="description">
+                            This is an example of React context using the
+                            reducer pattern
+                        </p>
+                        <LoadingSpinner />
+                        <p className="hint">
+                            Press the button to toggle between loading state
+                        </p>
+                        <button
+                            className="button"
+                            onClick={api.toggle}
+                            type="button"
+                        >
+                            Toggle Loading
+                        </button>
+                    </>
+                )}
+            </Context.Consumer>
         </Context.Provider>
     );
 }
