016fce8e6093ee428601bb87f913dbf6cdf03d4f
MenSeb <sebastien.menard.github@gmail.com>
2020-12-08 13:23:27 -0500

    update provider

--- src/provider.jsx
+++ src/provider.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useMemo, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import Context from './context';
+import { createAPI, createReducer } from './utilities';
 
-export default function Provider({ children }) {
-    return <Context.Provider value={{}}>{children}</Context.Provider>;
+export default function Provider({ actions, children, config, initial }) {
+    const [state, dispatch] = useReducer(
+        createReducer(actions),
+        initial,
+        config,
+    );
+
+    const api = useMemo(() => {
+        return createAPI(actions, dispatch);
+    }, [actions, dispatch]);
+
+    return (
+        <Context.Provider value={{ api, state }}>{children}</Context.Provider>
+    );
 }
 
-Provider.defaultProps = {};
+Provider.defaultProps = {
+    config: undefined,
+    initial: {},
+};
 
 Provider.propTypes = {
+    actions: PropTypes.objectOf(PropTypes.func).isRequired,
     children: PropTypes.node.isRequired,
+    config: PropTypes.func,
+    initial: PropTypes.object,
 };
