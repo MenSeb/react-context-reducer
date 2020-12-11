60cdcaf3ee5bee7f675c1ac6048cca52160421c0
MenSeb <sebastien.menard.github@gmail.com>
2020-12-11 01:39:36 -0500

    catch additional errors

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,10 @@
 const { error: consoleError } = console;
 
-export const ERROR_MESSAGES = /Invalid prop|Failed prop type|Failed test type/gi;
+export const ERROR_MESSAGES = /Invalid prop|Failed prop type|Failed test type|Unknown Context/gi;
 
 export function error(message, ...args) {
+    if (/The above error occurred in the/gi.test(message)) return;
+
     if (ERROR_MESSAGES.test(message)) {
         throw new Error(message);
     }
